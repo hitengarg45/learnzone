1b9ab60a716d83fc5ff84f963f2eda9754e3ea39
Hiten Garg <hitengarg45@gmail.com>
2021-07-25 10:52:47 +0530

    Learnzone portal

---
+++ learnzone/src/test/java/com/amdocs/learnzone/LearnzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amdocs.learnzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
