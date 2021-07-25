f37c94c08fa2ca46976d91c9e5b33ebd23774183
Hiten Garg <hitengarg45@gmail.com>
2021-07-25 21:36:20 +0530

    User Register implementation

--- learnzone/src/main/java/com/amdocs/learnzone/controllers/HomeController.java
+++ learnzone/src/main/java/com/amdocs/learnzone/controllers/HomeController.java
@@ -1,18 +1,34 @@
 package com.amdocs.learnzone.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@RestController
+import com.amdocs.learnzone.models.User;
+import com.amdocs.learnzone.services.UserService;
+
+@Controller
 public class HomeController {
-
+	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping("/")
 	public String homePage() {
-		return "Home Page";
+		return "index";
 	}
 	
 	@GetMapping("/register")
-	public String registerForm() {
-		return "Sign Up form";
+	public String registerForm(Model model) {
+		model.addAttribute("user", new User());
+		return "signup_form";
+	}
+	
+	@PostMapping("/register/add")
+	public String addUser(User user) throws Exception {
+		userService.addUser(user);
+	    return "register_success";
 	}
 }
---
+++ learnzone/src/main/java/com/amdocs/learnzone/services/UserService.java
@@ -0,0 +1,32 @@
+package com.amdocs.learnzone.services;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.amdocs.learnzone.models.User;
+import com.amdocs.learnzone.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+	
+	public void addUser(User user) throws Exception {
+//		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//	    String encodedPassword = passwordEncoder.encode(user.getPassword());
+//	    user.setPassword(encodedPassword);
+	    
+	    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");  
+		LocalDateTime now = LocalDateTime.now();  
+		Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dtf.format(now));  
+	    
+		user.setRegDate(date);
+		
+	    userRepository.save(user);
+	}
+}
